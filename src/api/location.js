f9420d79001d0d4134c93c5f046bde1b9be22c7a
tank <540153461@qq.com>
2019-06-10 15:52:22 +0800

    定位还没做完

--- src/api/location.js
+++ src/api/location.js
@@ -1,11 +1,11 @@
-//import axios from 'axios'
+import axios from 'axios'
 
 export function fetchLocation (){ //函数一定要有空格,设置你 的过期时间，时间太长了，咱们就撤销了。
 	let options={
 		timeout:5000
 	}
 	console.log('start')
-	return new Promise((resolve,reject)=>{
+	return new Promise((resolve, reject)=>{
 		navigator.geolocation.getCurrentPosition((pos)=>{
 			resolve(pos)
 		},(err)=>{
@@ -16,5 +16,18 @@ export function fetchLocation (){ //函数一定要有空格,设置你 的过期
 }
 
 
+//根据经纬度获取地址信息
+
+//export function getAddress(latitude, longitude){
+//	let url ='https://restapi.ele.me/bgs/poi/reverse_geo_coding'
+//	let params = {latitude , longitude}
+//	return new Promise((resolve, reject)=>{
+//		axios.get(url, {params}).then(res=>{
+//			resolve(res)
+//		}).catch(err=>{
+//			reject(err)
+//		})
+//	})
+//	
 
 
